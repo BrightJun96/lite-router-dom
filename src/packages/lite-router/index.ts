8dfef123785bd1651a748dc283d4ad39ba6cf9ec
jjalseu <cjfwns966@gmail.com>
2024-11-09 09:40:58 +0900

    - Route 컴포넌트 Init

---
+++ src/packages/lite-router/index.ts
@@ -0,0 +1,10 @@
+import React from "react";
+
+interface RouteProps {
+    children?: React.ReactNode;
+    component?: React.ReactNode;
+    path?: string;
+}
+export function Route(props:RouteProps):React.ReactNode|null{
+    return null;
+}
